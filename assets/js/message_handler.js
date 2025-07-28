4edecc7c1c0671ce20afedff070a75af699eb67b
jiemcode <dmax6176@gmail.com>
2025-07-28 20:50:28 +0000

    updates: skills section, theme toggler,  profile, ...

--- assets/js/message_handler.js
+++ assets/js/message_handler.js
@@ -36,7 +36,7 @@ $("#submitButtonId").click(function (event) {
     contentType: "application/json",
     data: jsonString,
     beforeSend: function (data) {
-      showSuccessMessage("Envi en cours !", "#286dc7");
+      showSuccessMessage("Envoi en cours !", "#286dc7");
     },
     success: function (data) {
       showSuccessMessage("Message envoyé ! Merci", "#4CAF50");
